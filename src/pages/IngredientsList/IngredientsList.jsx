615161cd9af68633b4d0a635e37b3d6254008442
safirapm <safira.paramita.m@gmail.com>
2023-03-01 18:00:45 +0700

    ingredients list ui

--- src/pages/IngredientsList/IngredientsList.jsx
+++ src/pages/IngredientsList/IngredientsList.jsx
@@ -25,24 +25,44 @@ const IngredientsList = () => {
   return (
     <>
       <Container className="ilist">
-        <div className="ilist-menu">
-          {allIng.map((ing) => {
-            return (
-              <Container key={ing.idIngredient}>
-                <a href={`/ingredients-detail/${ing.strIngredient}`}>
-                  <img
-                    src={`https://www.themealdb.com/images/ingredients/${ing.strIngredient}.png`}
-                    alt={ing.strIngredient}
-                  />
-                </a>
-                <p>{ing.strIngredient}</p>
-                <p>{ing.strDescription}</p>
-                <p>{ing.strDescription}</p>
-              </Container>
-            );
-          })}
+        <div className="ilist-header">
+          <h2>let's get cookin' and mealin'.</h2>
+          <label htmlFor="site-search">look for ingredients here:</label>
+          <input
+            type="search"
+            id="site-search"
+            placeholder="type your ingredient"
+          ></input>
+
+          <Button type="submit">Search</Button>
         </div>
-        <Button>Search</Button>
+
+        <Container className="ilist-section" id="ingredient">
+          <h5>click any ingredients of your choice below!</h5>
+          <Container className="ilist-all pd-10">
+            {allIng.map((ing) => {
+              return (
+                <Container key={ing.idIngredient} className="ilist-all">
+                  <a href={`/ingredients-detail/${ing.strIngredient}`}>
+                    <div className="card ingredient-card">
+                      <div className="ilist-img">
+                        <img
+                          src={`${process.env.REACT_APP_BASEURL}images/ingredients/${ing.strIngredient}.png`}
+                          alt={ing.strIngredient}
+                        />
+                      </div>
+                      <div className="card-img-overlay ingredient-overlay layer-ing">
+                        <h5 className="card-title layer-ing-title ingredient-title">
+                          {ing.strIngredient}
+                        </h5>
+                      </div>
+                    </div>
+                  </a>
+                </Container>
+              );
+            })}
+          </Container>
+        </Container>
       </Container>
     </>
   );
