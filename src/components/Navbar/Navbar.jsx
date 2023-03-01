1fdf433fdeee34bdfb01e54e085c720f753cc60c
safirapm <safira.paramita.m@gmail.com>
2023-03-02 01:34:25 +0700

    editing navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,27 +1,49 @@
-import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import "./Navbar.css";
 
 function WebNavbar() {
+  const [navbar, setNavbar] = useState(false);
+
+  const changeNavbar = () => {
+    if (window.scrollY >= 30) {
+      setNavbar(true);
+    } else {
+      setNavbar(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", changeNavbar);
+  }, []);
+
   return (
-    <Navbar fixed="top" bg="light" expand="lg">
+    <Navbar
+      fixed="top"
+      expand="lg"
+      className={navbar ? "navbar-active" : "navbar"}
+    >
       <Container>
-        <Navbar.Brand href="#home">Mealin' 'N Rollin'</Navbar.Brand>
+        <Navbar.Brand
+          href="/"
+          style={{
+            fontFamily: "'General Sans', sans-serif",
+            fontWeight: "600",
+          }}
+          className={navbar ? "color-active" : "font-color"}
+        >
+          mealin'.
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="justify-content-end flex-grow-1 pe-3"></Nav>
           <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/categories">Categories</Nav.Link>
-            <Nav.Link href="/random">Random Meal</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+            <Nav.Link
+              href="/#ingredient"
+              className={navbar ? "color-active" : "font-color"}
+            >
+              Ingredients
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
